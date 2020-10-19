fc9e5528f59b3f1f8c53b5d63a7b0add9078d05f
MorgothGorthaur <58920821+MorgothGorthaur@users.noreply.github.com>
2020-10-19 12:43:28 +0300

    Update Calendaryk.sql

--- documentation/Calendaryk.sql
+++ documentation/Calendaryk.sql
@@ -1,24 +1,24 @@
-CREATE	TABLE    calendars(
-			idCalendar TEXT  PRIMARY KEY NOT NULL,
-			name TEXT);
+CREATE  TABLE    calendars(
+      idCalendar TEXT  PRIMARY KEY NOT NULL,
+      name TEXT NOT NULL);
 
-CREATE	TABLE    events(
-			idEvent TEXT  PRIMARY KEY NOT NULL,
-			idCalendar TEXT,
-			description TEXT,
-			startTime TEXT,
-			endTime TEXT,
-			FOREIGN KEY(idCalendar) REFERENCES calendars(idCalendar));
-CREATE	TABLE    notifications(
-			idNotifcation TEXT PRIMARY KEY NOT NULL ,
-			idEvent TEXT ,
-			notificationTime TEXT,
-			FOREIGN KEY(idEvent) REFERENCES events(idEvent));
-CREATE	TABLE    sysdiagrams(
-			name VARCHAR(3),
-			principal_id INTEGER,
-			version INTEGER,
-			difinition TEXT);
+CREATE  TABLE    events(
+      idEvent TEXT  PRIMARY KEY NOT NULL,
+      idCalendar TEXT NOT NULL,
+      description TEXT NOT NULL,
+      startTime TEXT NOT NULL,
+      endTime TEXT NOT NULL,
+      FOREIGN KEY(idCalendar) REFERENCES calendars(idCalendar));
+CREATE  TABLE    notifications(
+      idNotifcation TEXT PRIMARY KEY NOT NULL ,
+      idEvent TEXT NOT NULL,
+      notificationTime TEXT NOT NULL,
+      FOREIGN KEY(idEvent) REFERENCES events(idEvent));
+CREATE  TABLE    sysdiagrams(
+      name VARCHAR(3),
+      principal_id INTEGER  PRIMARY KEY NOT NULL,
+      version INTEGER NOT NULL,
+      definition TEXT NOT NULL);
 			
 
 
