5c68a0f1cbd375bc6345f0381041b6caf7dba00d
oleksijpasternak <57842967+oleksijpasternak@users.noreply.github.com>
2020-10-27 14:52:42 +0200

    Add files via upload

---
+++ documentation/Script.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE Calendars
+(
+    id INT IDENTITY PRIMARY KEY ,
+    name VARCHAR(20)
+);
+
+CREATE TABLE Events
+(
+    id INT IDENTITY PRIMARY KEY,
+    idCalendar INT,
+    description VARCHAR(50),
+    startTime SMALLDATETIME,
+    endTime SMALLDATETIME,
+    FOREIGN KEY(idCalendar) REFERENCES Calendars(id)
+);
+
+CREATE TABLE Notifications
+(
+    id INT IDENTITY,
+    idEvent INT ,
+    notificationTime SMALLDATETIME,
+    FOREIGN KEY(idEvent) REFERENCES Events(id)
+);
